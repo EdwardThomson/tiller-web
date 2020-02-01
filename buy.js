91e1314a725874294c83bb1ef3cd761ad7c8a5fa
Edward Thomson <thomson.ed@gmail.com>
2020-02-02 07:53:44 +1100

    setting step number

--- buy.js
+++ buy.js
@@ -48,7 +48,7 @@ $(document).ready(async function () {
     var qtySilver;
     var qtyOrange;
 
-    var step;
+    var step = 1;
 
     const colourSelectBlack = $('#lrw-id-checkout__colour-select--black');
     const colourSelectGrey = $('#lrw-id-checkout__colour-select--grey');
