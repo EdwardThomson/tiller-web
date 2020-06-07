9fd068fb5ebfbe10da1dc7f98a058757a2b388e9
Edward Thomson <thomson.ed@gmail.com>
2020-06-07 23:10:03 +1000

    Updating pricing page numbers

--- cart-staging.js
+++ cart-staging.js
@@ -213,6 +213,8 @@ $(document).ready(async function () {
         productPrice = parseFloat(((qtyTotal * devicePrice)).toFixed(2));
         subtotalPrice = productPrice + subscriptionPrice;
 
+        $('#lrw-id-price-device-and-plan').text(`$${devicePrice} + ${planPrice}/month`);
+
         $('#lrw-id-checkout__qty--black').val(qtyBlack);
         $('#lrw-id-checkout__qty--grey').val(qtyGrey);
         $('#lrw-id-checkout__qty--silver').val(qtySilver);
